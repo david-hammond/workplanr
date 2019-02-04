4d5e6ac97ffdb17498d49169179b2a9cc3fdf57c
david-hammond <dhammond@economicsandpeace.org>
2019-02-04 22:26:11 +1100

    Finish Documentation

--- R/14-workplanr.R
+++ R/14-workplanr.R
@@ -8,12 +8,12 @@
 #' @importFrom dplyr "%>%"
 #' @importFrom grDevices grey
 #' @importFrom stats end na.omit start
+#' @importFrom utils vignette
 NULL
 
-## quiets concerns of R CMD check re: the .'s that appear in pipelines
 if(getRversion() >= "2.15.1")  utils::globalVariables(c("assigned_capacity", "capacity", "date.type", "description",
                                                           "holiday_expansion_factor", "leave_adjusted_workload",
                                                           "leave_expansion_factor", "maxcapacity", "mutate", "name", "needed", 
                                                           "new", "num_holidays", "num_out_of_office", "out_of_office", "phase", "probability",
                                                           "project", "project_duration", "public_holiday", "responsibilities", "staff",
-                                                          "teamload", "time_estimate", "workload"), add = FALSE)
+                                                          "teamload", "time_estimate", "workload", "jurisdiction", "holiday.name"), add = FALSE)
