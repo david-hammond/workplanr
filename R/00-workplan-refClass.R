aa2cde581d051cbb30783ce1a25aebd2bd70de39
david-hammond <dhammond@economicsandpeace.org>
2019-02-17 18:38:57 +1100

    updating plots

--- R/00-workplan-refClass.R
+++ R/00-workplan-refClass.R
@@ -169,17 +169,26 @@ workplan <- R6Class("workplan",
   printStaffSchedule = function(){
     tmp <- self$wp_schedule$staff_schedule
     tmp <- tmp %>%
-      dplyr::filter(date <= max(date[!is.na(workload)]))
-    myPalette <- grDevices::colorRampPalette(RColorBrewer::brewer.pal(11, 'RdGy')[c(6,2)], 
-                                             space='Lab')
+      dplyr::filter(date <= max(date[!is.na(workload)]), 
+                    !is.na(workload))
+    #TODO: make function calc beaks
+    bins <- c(0,0.25, 0.5, 0.75, 1, 1.25, 2, max(tmp$workload))
+    labels <- c("[0 - 25%]", "[25% - 50%]", "[50% - 75%]", "[75% - 100%]", "[100% - 125%]",
+                "[125% - 200%]", paste0("[200% - ", round(100*max(tmp$workload),0), "%]"))
+    work_classes <- classInt::classIntervals(tmp$workload, n = length(bins)-1, style = "fixed",
+                                             fixedBreaks = bins)
+    tmp$workload = cut(tmp$workload, bins, labels = labels)
+    tmp$workload = forcats::fct_rev(tmp$workload)
+    main_title <- paste0("Planned Staff Workload - ", format(lubridate::today(), "%d %B %Y"))
+    # labels = scales::percent, 
     p <- ggplot2::ggplot(data = tmp, 
                          ggplot2::aes(date, staff_name, fill = workload)) +
-      ggplot2::geom_tile(alpha = 0.9) +
-      ggplot2::scale_fill_gradientn(colors = myPalette(100), 
-                                    labels = scales::percent, name = 'Workload', na.value = "white",
-                                    limits = c(0, max(tmp$workload))) +
-      ggplot2::labs(x='', y = '', 
-                    title = toupper('STAFF WORKLOAD')) +
+      ggplot2::geom_tile(alpha = 0.6) +
+      ggplot2::scale_fill_manual(values = 
+                                   rev(RColorBrewer::brewer.pal(n = max(length(unique(tmp$workload)),3), 
+                                                                "Reds")), 
+                                 name = 'Workload', na.value = "white") +
+      ggplot2::labs(x='', y = '', title = main_title) +
       ggplot2::scale_x_date(labels = scales::date_format('%b'), 
                             date_breaks = '1 month', 
                             expand = c(0,0))
@@ -189,14 +198,15 @@ workplan <- R6Class("workplan",
       dplyr::filter(!is.na(project_name))
     p <- p + ggrepel::geom_text_repel(data = project_labels, 
                                       ggplot2::aes(x = date, y = staff_name, label = project_name), 
-                                      size = 3, hjust = 1)   
+                                      size = 3, hjust = 1)   +
+      ggplot2::geom_vline(xintercept = lubridate::today(), colour = "red", linetype = "dashed")
     
     #add leave
     
     leave <- tmp %>%
       dplyr::group_by(staff_name, id_out_of_office, out_of_office) %>%
       dplyr::summarise(start = min(date), end = max(date), 
-                       workload = mean(workload, na.rm = T)) %>%
+                       workload = tmp$workload[1]) %>%
       dplyr::filter(!is.na(out_of_office))
     
     p <- p + ggplot2::geom_segment(data = leave, ggplot2::aes(x=start, 
@@ -204,9 +214,11 @@ workplan <- R6Class("workplan",
                                                               y=staff_name, 
                                                               yend=staff_name, colour = out_of_office), size=2, alpha = 0.6)
     p <- p + ggplot2::geom_point(data = leave, ggplot2::aes(x=start, 
-                                                            y=staff_name, colour = out_of_office), size=3)
+                                                            y=staff_name, colour = out_of_office), size=3,
+                                 show.legend = FALSE)
     p <- p + ggplot2::geom_point(data = leave, ggplot2::aes(x=end, 
-                                                            y=staff_name, colour = out_of_office), size=3)
+                                                            y=staff_name, colour = out_of_office), size=3,
+                                 show.legend = FALSE)
     p <- p + ggplot2::labs(fill ="Workload" ,colour="Out of Office")
     public_holidays <- tmp %>% 
       dplyr::filter(!is.na(holiday_name)) 
